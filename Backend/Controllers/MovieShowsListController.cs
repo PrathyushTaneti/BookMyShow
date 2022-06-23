a06d4df6981b0d33f04b143df1536c56655aa24f
Prathyush Taneti <prathyushtaneti.11@gmail.com>
2022-06-23 10:16:42 +0530

    Add files via upload

---
+++ Backend/Controllers/MovieShowsListController.cs
@@ -0,0 +1,49 @@
+﻿using BookMyShow.Models;
+using BookMyShow.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMyShow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieShowsListController : ControllerBase
+    {
+        private readonly IMovieShowsListService movieShowListService;
+
+        public MovieShowsListController(IMovieShowsListService movieShowListService)
+        {
+            this.movieShowListService = movieShowListService;
+        }
+
+        [HttpGet]
+        public List<MovieShowsList> Get()
+        {
+            return this.movieShowListService.GetAllShows();
+        }
+
+        [HttpGet("id")]
+        public MovieShowsList Get(int id)
+        {
+            return this.movieShowListService.GetShowDetailById(id);
+        }
+
+        [HttpPost]
+        public int Post(MovieShowsList movieShow)
+        {
+            return this.movieShowListService.CreateShow(movieShow);
+        }
+
+        [HttpPut("id")]
+        public bool Put(int id, MovieShowsList movieShow)
+        {
+            return this.movieShowListService.UpdateShow(id, movieShow);
+        }
+
+        [HttpDelete("id")]
+        public bool Delete(int id)
+        {
+            return this.movieShowListService.DeleteShow(id);
+        }
+    }
+}
---
+++ Backend/Controllers/UserDetailController.cs
@@ -0,0 +1,49 @@
+﻿using BookMyShow.Models;
+using BookMyShow.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMyShow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailController : ControllerBase
+    {
+        private readonly IUserDetailService userDetailService;
+
+        public UserDetailController(IUserDetailService userDetailService)
+        {
+            this.userDetailService = userDetailService;
+        }
+
+        [HttpGet]
+        public List<UserDetail> Get()
+        {
+            return this.userDetailService.GetAllUserDetails();
+        }
+
+        [HttpGet("id")]
+        public UserDetail Get(int id)
+        {
+            return this.userDetailService.GetUserDetailById(id);
+        }
+
+        [HttpPost]
+        public int Post (UserDetail userDetail)
+        {
+            return this.userDetailService.CreateUserDetail(userDetail);
+        }
+
+        [HttpPut("id")]
+        public bool Put(int id, UserDetail userDetail)
+        {
+            return this.userDetailService.UpdateUserDetail(id, userDetail);
+        }
+
+        [HttpDelete("id")]
+        public bool Delete(int id)
+        {
+            return this.userDetailService.DeleteUserDetail(id);
+        }
+    }
+}
